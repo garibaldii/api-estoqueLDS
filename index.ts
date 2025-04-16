b6955eca69f845d94173cde621a7ba1ee74cc327
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-04-16 09:57:02 -0300

    🎉chore: first commit
    - Added package.json with dependencies and devDependencies for TypeScript, Express, and MongoDB.
    - Created tsconfig.json with compiler options for TypeScript configuration.

---
+++ index.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import dotenv from 'dotenv'
+
+import pedidoRouter from './src/Routes/Pedido';
+import inversorRouter from './src/Routes/Produtos/Inversor';
+import painelRouter from './src/Routes/Produtos/Painel'
+
+import conectaBancoDados from './src/Database/db';
+import { errorHandler } from './src/Middleware/ErrorHandler';
+import { autenticarToken } from './src/Middleware/Auth';
+import authRouter from './src/Routes/Auth';
+
+dotenv.config()
+
+const app = express()
+const port = 3000;
+
+conectaBancoDados()   
+
+app.use(express.json())
+
+app.use("/inversor", autenticarToken ,inversorRouter )
+app.use("/conta", authRouter)
+app.use("/painel", painelRouter )
+app.use("/pedido", pedidoRouter)
+
+app.use(errorHandler)
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port} 🚪`)
+})

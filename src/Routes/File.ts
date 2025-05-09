effeb8ff65d084fa1a8124e460a5a8422eec7f40
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-05-09 11:58:46 -0300

    ✨feat: implement file upload and download functionality with multer

---
+++ src/Routes/File.ts
@@ -0,0 +1,43 @@
+import { Router } from 'express'
+import path from 'path'
+import multer from 'multer'
+import { uploadProductFile } from '../Service/Excel'
+
+
+const fileRouter = Router()
+
+
+fileRouter.get("/download", (req, res, next) => {
+    //get the excel model
+    try {
+        const filePath = path.join(__dirname, "../../public/text.txt")
+        res.download(filePath, "text.txt")
+    } catch (error) {
+        next(error)
+    }
+})
+
+const upload = multer({ storage: multer.memoryStorage() })
+fileRouter.post("/upload", upload.single("file"), async (req, res, next) => {
+    try {
+
+        const buffer = req.file?.buffer
+
+        if (!buffer) {
+            res.status(400).json({ message: "Arquivo nao encontrado" })
+            return
+        }
+
+        const response = await uploadProductFile(buffer)
+
+        res.status(201).send({
+            message: "Produtos Cadastrados com Sucesso! 🥳🎉",
+            response
+        })
+
+    } catch (error) {
+        next(error)
+    }
+})
+
+export default fileRouter
--- src/Service/Excel.ts
+++ src/Service/Excel.ts
@@ -1,5 +1,8 @@
 import * as xlsx from 'xlsx';
 import * as path from 'path';
+import { criaInversorEmLote } from './Produtos/Inversor';
+import { criaPainelEmLote } from './Produtos/Painel';
+import { HttpError } from '../Utils/HttpError';
 
 export const lerRegistrosExcel = (): { produto: object, dataCriacao: string }[] => {
     const caminhoArquivo = path.join(__dirname, '../../../estoqueLDS.xlsx');
@@ -14,10 +17,9 @@ export const lerRegistrosExcel = (): { produto: object, dataCriacao: string }[] 
     const produtos: { produto: object, dataCriacao: string }[] = [];
     const horarioCriacao = new Date()
 
-
     infos.forEach((linha, index) => {
         if (index > 1 && Array.isArray(linha)) {
-           // const modelo = String(linha[0]);
+            // const modelo = String(linha[0]);
             //const codigoDeBarras = String(linha[1]);
             const produto = {
                 modelo: String(linha[0]),
@@ -26,9 +28,44 @@ export const lerRegistrosExcel = (): { produto: object, dataCriacao: string }[] 
 
             const dataCriacao = `${horarioCriacao.toLocaleDateString('pt-BR')} || ${horarioCriacao.toLocaleTimeString("pt-BR")}`
 
-            produtos.push({ produto, dataCriacao});
+            produtos.push({ produto, dataCriacao });
         }
     });
 
     return produtos;
-}; 
+};
+
+
+export const uploadProductFile = async (fileBuffer: Buffer) => {
+    const workBook = xlsx.read(fileBuffer, { type: "buffer" });
+    const errors: any[] = [];
+
+    const extractSheetData = (sheetName: string) => {
+        const sheet = workBook.Sheets[sheetName];
+        return xlsx.utils.sheet_to_json<any[]>(sheet, { header: 0 });
+    };
+
+    const panelData = extractSheetData(workBook.SheetNames[0]);
+    const inverterData = extractSheetData(workBook.SheetNames[1]);
+
+    try {
+        if (inverterData.length) await criaInversorEmLote(inverterData);
+    } catch (e: any) {
+        errors.push({"Inversores": e.message});
+    }
+
+    try {
+        if (panelData.length) await criaPainelEmLote(panelData);
+    } catch (e: any) {
+        errors.push({"Paineis": e.message});
+    }
+
+    if (errors.length) throw new HttpError("Código de Barras Duplicado", 409, errors)
+
+    return { inverterData, panelData };
+};
+
+
+
+
+
